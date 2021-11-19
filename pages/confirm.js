1f06e7d2b3bfce2354b2c4a8b645a78feafa0171
Darbaz Ali <darbaz.me@gmail.com>
2021-11-19 21:12:22 +0300

    back to search button

--- pages/confirm.js
+++ pages/confirm.js
@@ -2,19 +2,23 @@ import tw from "tailwind-styled-components"
 import Map from "../components/Map"
 import RideSelector from "../components/RideSelector"
 import { useRouter } from "next/router"
-
+import { BsArrowLeft } from "react-icons/bs"
+import Link from "next/link"
 const Confirm = () => {
     const router = useRouter()
     const { pickup, dropoff } = router.query
     return (
         <Wrapper>
+            <Link href='/search' passHref>
+                <BackButton>
+                    <BsArrowLeft size={32} />
+                </BackButton>
+            </Link>
+
             <Map pickupCoords={pickup} dropoffCoords={dropoff} />
 
             <RideContainer>
-                {/* Ride Seelector */}
                 <RideSelector />
-                {/* Confirm Button */}
-
                 <ConfirmButtonContainer>
                     <ConfirmButton>Confirm UberX</ConfirmButton>
                 </ConfirmButtonContainer>
@@ -24,7 +28,7 @@ const Confirm = () => {
 }
 
 const Wrapper = tw.div`
-    flex h-screen flex-col overflow-y-hidden
+    flex h-screen flex-col overflow-y-hidden relative
 `
 
 const RideContainer = tw.div`
@@ -37,6 +41,11 @@ const ConfirmButton = tw.button`
     w-full py-4 px-6 my-4 text-blue-100 
     transition-colors duration-150 bg-blue-700 
     rounded-lg focus:shadow-outline hover:bg-blue-800
+`
+
+const BackButton = tw.button`
+    absolute z-50 inset-2 bg-blue-100 h-12 w-12 
+    p-2 rounded-full text-blue-700 opacity-90
 `
 
 export default Confirm
